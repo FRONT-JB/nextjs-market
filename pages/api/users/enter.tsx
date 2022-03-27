c90d8f5538e2fcf7923005b6de71f0b19e044550
전병길 <jeonbg@kakao.com>
2022-03-28 00:35:51 +0900

    #9.2 : Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,17 +6,37 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
 
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  // User를 조회하는 영역
+  // const user = await client.user.upsert({
+  //   where: {
+  //     ...payload,
+  //   },
+  //   create: {
+  //     name: 'Anonymous',
+  //     ...payload,
+  //   },
+  //   update: {},
+  // });
+
+  // 이곳에서 위의 유저를 조회한다.
+  // 조회 후에 유저정보가 있으면 Token을 생성
+  const token = await client.token.create({
+    data: {
+      payload: '1234',
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: 'Anonymous',
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: 'Anonymous',
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+
   return res.status(200).end();
 }
 
